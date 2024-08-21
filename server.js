c61c94b64355aa45f6399c1b2a5ac6574ed459d2
dwjrgal <dwjrgl651@gmail.com>
2024-08-21 21:06:57 +0800

    cors

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
-
 const fs = require("fs");
+const cors = require("cors");
 
 const app = express();
-const cors = cors();
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/users", (req, res) => {
